bdfb79d6480e80badf809b424024b2663703a0de
lan9 <lang@yuzelang.com>
2018-08-06 23:55:17 -0700

    wire up build file with react client

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express();
 const path = require("path");
 
 app.get("/", (req, res) =>
-	res.sendFile(path.join(__dirname + "/client/index.html"))
+	res.sendFile(path.join(__dirname, "client/build/index.html"))
 );
 
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.listen(3000, ()=> console.log("Server started"));
