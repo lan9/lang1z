ab494da3144b0ac132209e4a770b3e292eb76659
lan9 <lang@yuzelang.com>
2018-08-13 23:25:39 -0700

    Add social media links

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
-import './App.css';
+import './styles/App.css';
+import twitterLogo from './images/twitter.svg'
+import linkedInLogo from './images/linkedin.svg'
 
 class App extends Component {
     constructor(props, context) {
@@ -21,29 +23,41 @@ class App extends Component {
         this.setState({width: width, height: height});
 
     }
+
     componentWillMount() {
         this.updateDimensions();
     }
+
     componentDidMount() {
         window.addEventListener("resize", this.updateDimensions.bind(this));
     }
+
     componentWillUnmount() {
         window.removeEventListener("resize", this.updateDimensions.bind(this));
     }
 
     render() {
         const wideMode = window.innerWidth > 500;
-
         return (
 
             <div className="App-backdrop">
                 <div className="App-content">
                     <div className="App-title">Hello I'm Yuze.</div>
                     I'm a software engineer at Twitter.
+                    <div className="App-bottom-bar">
+                        <Icon url="https://twitter.com/lang1z" logo={twitterLogo}/>
+                        <Icon url="https://linkedin.com/in/yuzelang" logo={linkedInLogo}/>
+                    </div>
                 </div>
             </div>
         );
     }
+}
+
+function Icon(props) {
+    return <span className="App-icon">
+          <a target="_blank" href={props.url}><img src={props.logo}/></a>
+        </span>;
 }
 
 export default App;
