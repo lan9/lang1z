b702db12db78ee57cfb9d59988a6ed14fc5933a8
lan9 <lang@yuzelang.com>
2018-09-06 23:22:53 -0700

    Add proptypes check

--- client/src/Card.js
+++ client/src/Card.js
@@ -1,16 +1,34 @@
 import styled from 'styled-components';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export default styled.div`
+class Card extends React.PureComponent {
+  render() {
+    return <StyledDiv {...this.props} />;
+  }
+}
+
+Card.propTypes = {
+  id: PropTypes.number,
+  init: PropTypes.bool,
+  top: PropTypes.string,
+  opacity: PropTypes.number,
+  textColor: PropTypes.string,
+  bgColor: PropTypes.string
+};
+const StyledDiv = styled.div`
   position: absolute;
   color: ${props => props.theme.textColor};
   opacity: ${props => (props.init ? '0' : '1')};
   transition: 0.5s 0.3s;
   height: 200px;
   width: 100%;
-  top: ${props => (props.top === undefined ? 0 : props.top)};
+  top: ${props => (props.top === undefined ? '0' : props.top)};
   opacity: ${props => (props.opacity === undefined ? 1 : props.opacity)};
   background-color: ${props => props.theme.bgColor};
   border-radius: 1.5rem;
   padding: 50px;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 `;
+
+export default Card;
